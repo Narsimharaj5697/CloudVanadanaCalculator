e3ad2e8ce94fbcfd62d466bb0c6e96ea081ef77a
Narsimharaj5697 <114253108+Narsimharaj5697@users.noreply.github.com>
2023-10-15 19:26:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,113 @@
+const themeIcon = document.getElementById("theme-icon");
+const res = document.getElementById("result");
+
+const txts = document.querySelector(".animate-text").children,
+  txtsLen = txts.length;
+let index = 0;
+const textInTimer = 3000,
+  textOutTimer = 2800;
+
+function animateText() {
+  for (let i = 0; i < txtsLen; i++) {
+    txts[i].classList.remove("text-in", "text-out");
+  }
+  txts[index].classList.add("text-in");
+
+  setTimeout(function () {
+    txts[index].classList.add("text-out");
+  }, textOutTimer);
+
+  setTimeout(function () {
+    if (index == txtsLen - 1) {
+      index = 0;
+    } else {
+      index++;
+    }
+    animateText();
+  }, textInTimer);
+}
+
+window.onload = animateText;
+
+function calculate(value) {
+  const calculatedValue = eval(value || null);
+  if (isNaN(calculatedValue)) {
+    res.value = "Can't divide 0 with 0";
+    setTimeout(() => {
+      res.value = "";
+    }, 1300);
+  } else {
+    res.value = calculatedValue;
+  }
+}
+
+function liveScreen(enteredValue) {
+  if (!res.value) {
+    res.value = "";
+  }
+  res.value += enteredValue;
+}
+
+//adding event handler on the document to handle keyboard inputs
+document.addEventListener("keydown", keyboardInputHandler);
+
+//function to handle keyboard inputs
+function keyboardInputHandler(e) {
+  // to fix the default behavior of browser,
+  // enter and backspace were causing undesired behavior when some key was already in focus.
+  e.preventDefault();
+  //grabbing the liveScreen
+
+  //numbers
+  if (e.key === "0") {
+    res.value += "0";
+  } else if (e.key === "1") {
+    res.value += "1";
+  } else if (e.key === "2") {
+    res.value += "2";
+  } else if (e.key === "3") {
+    res.value += "3";
+  } else if (e.key === "4") {
+    res.value += "4";
+  } else if (e.key === "5") {
+    res.value += "5";
+  } else if (e.key === "6") {
+    res.value += "6";
+  } else if (e.key === "7") {
+    res.value += "7";
+  } else if (e.key === "7") {
+    res.value += "7";
+  } else if (e.key === "8") {
+    res.value += "8";
+  } else if (e.key === "9") {
+    res.value += "9";
+  }
+
+  //operators
+  if (e.key === "+") {
+    res.value += "+";
+  } else if (e.key === "-") {
+    res.value += "-";
+  } else if (e.key === "*") {
+    res.value += "*";
+  } else if (e.key === "/") {
+    res.value += "/";
+  }
+
+  //decimal key
+  if (e.key === ".") {
+    res.value += ".";
+  }
+
+  //press enter to see result
+  if (e.key === "Enter") {
+    calculate(result.value);
+  }
+
+  //backspace for removing the last input
+  if (e.key === "Backspace") {
+    const resultInput = res.value;
+    //remove the last element in the string
+    res.value = resultInput.substring(0, res.value.length - 1);
+  }
+}
